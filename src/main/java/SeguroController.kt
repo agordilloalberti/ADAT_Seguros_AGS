6f9ba1b5a25eeee7b6578a940426b2a90f0ce836
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-15 20:51:19 +0100

    Desde Clase

--- src/main/java/SeguroController.kt
+++ src/main/java/SeguroController.kt
@@ -1,6 +1,7 @@
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.Date
 
 @RestController
 @RequestMapping("/seguros")
@@ -27,6 +28,24 @@ class SeguroController() {
 
     @DeleteMapping("/{id}")
     fun deleteSeguro(@PathVariable id: Int):ResponseEntity<String>{
+        seguroService.deleteSeguro(id)
         return ResponseEntity.noContent().build()
+    }
+
+    @PutMapping("/{id}")
+    fun editSeguro(
+        @PathVariable id : Int,
+        @PathVariable nif : String,
+        @PathVariable nombre : String,
+        @PathVariable ape1 : String,
+        @PathVariable ape2 : String,
+        @PathVariable edad : Int,
+        @PathVariable hijos : Int,
+        @PathVariable fecha : Date,
+        @PathVariable sexo : String,
+        @PathVariable casado : Boolean,
+        @PathVariable embarazada : Boolean
+    ): ResponseEntity<String>{
+        return ResponseEntity.ok(seguroService.editSeguro(id,nif,nombre,ape1,ape2,edad,hijos,fecha,sexo,casado,embarazada))
     }
 }
